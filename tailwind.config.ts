5cfe1d9475b9309fcef35fe0ca80711b420f864b
Matheus Viana <matheusmviana@outlook.com.br>
2025-06-09 02:22:21 -0300

    feat: Update build process and integrate Tailwind CSS with PostCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -9,6 +8,4 @@ const config: Config = {
     },
   },
   plugins: [],
-};
-
-export default config;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 import ViteSitemap from "vite-plugin-sitemap"
 import { createHtmlPlugin } from "vite-plugin-html"
@@ -8,7 +7,6 @@ import { createHtmlPlugin } from "vite-plugin-html"
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     ViteSitemap({
       hostname: "http://localhost:3000",
       generateRobotsTxt: true,
